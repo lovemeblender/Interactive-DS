ed68ac9935bf7ef13789285bfa643f090b91ad34
Michael Despotopoulos <michaeldespoto@gmail.com>
2014-12-01 02:03:39 +0200

    Edit js css

--- list.js
+++ list.js
@@ -1,39 +1,37 @@
 var nodes = 0;
 var lastX = 0;
 
-function Node(value, next) {
-	this.value = value;
-	this.next = next;
-}
-
 function drawNode(context, x, y, value) {
 
 	<!--Draw node-->
 	context.strokeStyle = "black";
 	context.lineWidth = 1;
-	context.strokeRect(x, y, 140, 80);
+	context.strokeRect(x, y, 120, 75);
 
 	<!--Draw value box-->
 	context.strokeStyle = "black";
 	context.lineWidth = 1;
-	context.strokeRect(x + 80, y + 10, 40, 20);
+	context.strokeRect(x + 80, y + 10, 33, 20);
+	
+	// center value
 	if(value > 999) {
 		value = 999;
 	}
 	if(value.length == 1) {
-		context.fillText(value, x + 97, y + 23);
-	}
-	else if(value.length == 2) {
 		context.fillText(value, x + 95, y + 23);
 	}
-	else {
-		context.fillText(value, x + 92, y + 23);
+	else if(value.length == 2) {
+		context.fillText(value, x + 93, y + 23);
 	}
+	else {
+		context.fillText(value, x + 91, y + 23);
+	}
+	// center value
 
 	<!--Draw next node box-->
 	context.strokeStyle = "black";
 	context.lineWidth = 1;
-	context.strokeRect(x + 80, y + 40, 40, 20);
+	context.strokeRect(x + 80, y + 40, 33, 20);
 };
 
 function deleteNode(context, x, y) {
@@ -46,8 +44,6 @@ function deleteNode(context, x, y) {
 
 	<!--Delete next node box-->
 	context.clearRect(x + 80, y + 40, 40, 20);
-
-	// requestAnimationFrame(deleteNode);
 };
 
 function insertAtBack() {
@@ -59,40 +55,38 @@ function insertAtBack() {
 	}
 
     //var value = document.getElementById('insfid').value;
-    var value = Math.floor((Math.random() * 100) + 1); // Random integer [1-100]
-    var canvas = document.getElementById("canvas");
-	var context = canvas.getContext("2d");
+    var value = Math.floor((Math.random() * 100) + 1); // Value of the node, random integer [1-100]
+    var canvas = document.getElementById("canvas"); // get canvas
+	var context = canvas.getContext("2d"); // get context
 
-	if(nodes == 7) {
+	if(nodes == 7) { // draw the 7th node on the 2nd row
 		drawNode(context, lastX, 170, value);
 		context.moveTo(1150, 110);
  		context.lineTo(1150, 170);
 		context.strokeStyle="#black";
-		context.stroke();
-		context.closePath();
-		drawArrowhead(context, 1150, 170, true);
+		//drawArrowhead(context, 1150, 170, true);
 	}
 	else if(nodes > 7) {
-		drawArrow(context, lastX + 100, 220);
+		drawLine(context, lastX + 100, 220);
 		lastX -= 200;
 		drawNode(context, lastX, 170, value);
 	}
 
-	if(nodes == 1) {
-		lastX = 50;
-		drawNode(context, 50, 60, value);
-		context.fillText("value:", 68, 82);
-		context.fillText("next:", 68, 113);
+	if(nodes == 1) { // draw the first node at (20, 60)
+		lastX = 20;
+		drawNode(context, lastX, 60, value);
+		context.fillText("value:", 65, 82); // add value label
+		context.fillText("next:", 65, 113); // add next label
 	}
 	else if(nodes > 1 && nodes < 7) {
 		lastX += 200;
-		drawArrow(context, lastX, 110);
+		drawLine(context, lastX, 110);
 		drawNode(context, lastX, 60, value);
 	}
 
 	/*
 	*
-	*   TODO - OBJECTS CREATION
+	*   TODO - OOP APPROACH
 	*/
 
 	// Create Node Object
@@ -113,7 +107,7 @@ alert("Inserted " + value);
 	}
 	else if(nodes > 1 && nodes < 7) {
 		lastX += 200;
-		drawArrow(context, lastX, 110);
+		drawLine(context, lastX, 110);
 		drawNode(context, lastX, 60, value);
 	}
 
@@ -135,13 +129,12 @@ function deleteFromFront() {
 	var context = canvas.getContext("2d");
 
     deleteNode(context, 50, 60);
-    deleteArrow(context);
+    //deleteArrow(context);
     nodes--;
 };
 
 function deleteFromBack() {
     var value = document.getElementById('delbid').value;
-    
     nodes--;
 };
 
@@ -149,29 +142,23 @@ function initCanvas() {
 	var canvas = document.getElementById("canvas");
 	var context = canvas.getContext("2d");
 
-	nodes = 0;
-    lastX = 0;
+	nodes = 0; // number of nodes active on the canvas
+    lastX = 0; // position on the x axis
 
 	<!--Draw outer frame-->
 	context.strokeStyle = "#6E0000";
 	context.lineWidth = 5;
-	context.strokeRect(0, 0, 1200, 300);
+	context.strokeRect(0, 0, 1150, 300);
 
-	<!--Draw init node reference-->
+	<!--Draw head-->
 	context.strokeStyle = "black";
 	context.lineWidth = 1;
-	context.strokeRect(60, 10, 40, 20);
-	context.fillText("head", 68, 22);
+	context.strokeRect(20, 10, 40, 20);
+	context.fillText("head", 28, 23);
 
-	<!--Draw init vertical arrow-->
-	context.moveTo(80, 30);
- 	context.lineTo(80, 60);
-	context.strokeStyle="#black";
-	context.stroke();
-	context.closePath();
-	drawArrowhead(context, 80, 60, true);
-
-    //deleteNode(context, 250, 100);
+	<!--Draw head vertical arrow-->
+	drawVerticalLine(context, 30, 60);
+	//drawArrowhead(context, 80, 60, true);
 };
 
 /*
@@ -179,19 +166,30 @@ function initCanvas() {
 *
 */
 
+/*
 function deleteArrow(context) {
 	context.clearLine(250, 110);
 }
+*/
 
-function drawArrow(context, x, y) {
+function drawVerticalLine(context, x, y) {
+	context.beginPath();
+	context.moveTo(40, x);
+ 	context.lineTo(40, y);
+	context.strokeStyle="#black";
+	context.stroke();
+}
+
+function drawLine(context, x, y) {
+	context.beginPath();
 	context.moveTo(x - 100, y);
  	context.lineTo(x, y);
 	context.strokeStyle = "#black";
 	context.stroke();
-	context.closePath();
-    drawArrowhead(context, x, y);
+    //drawArrowhead(context, x, y);
 };
 
+/*
 function drawArrowhead(ctx, x, y, vertical) {
     ctx.save();
     ctx.beginPath();
@@ -209,3 +207,4 @@ function drawArrowhead(ctx, x, y, vertical) {
     ctx.restore();
     ctx.fill();
 };
+*/
